a917a9025afecfdea22e9241dc8943ec5cb2e123
andrii-pozniak <andriipozniak@gmail.com>
2023-06-07 23:36:43 +0300

    add pagination

--- src/components/CardsProduct/CardsProduct.jsx
+++ src/components/CardsProduct/CardsProduct.jsx
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { ProductCard } from "../ProductCard/ProductCard";
-// import { useParams } from "react-router-dom";
 import { fetchProductCards } from "../../Utils/api/getCard";
 import { CardProduct, Header } from "./CardsProduct.style";
+import { CustomPagination } from "../CustomPagination/CustomPagination";
 
 export const CardsProducts = () => {
-  // const { additionalCategory } = useParams();
   const [, setStatus] = useState("pending");
   const [data, setData] = useState(null);
   const [discountedProducts, setDiscountedProducts] = useState([]);
   const [bestSellingProducts, setBestSellingProducts] = useState([]);
   const [newArrivals, setNewArrivals] = useState([]);
+  const [currentPage, setCurrentPage] = useState({
+    discounted: 1,
+    bestSelling: 1,
+    newArrivals: 1,
+  });
+  const productsPerPage = 4;
 
   useEffect(() => {
     (async () => {
@@ -27,31 +32,55 @@ export const CardsProducts = () => {
   }, []);
 
   useEffect(() => {
-    if (data ) {
-      const items  = data;
-      const discounted = items?.filter(
-        (item) => item.additionalCategory.includes("Кращі цінові пропозиції")
+    if (data) {
+      const items = data.products;
+      const discounted = items?.filter((item) =>
+        item.additionalCategory.includes("Кращі цінові пропозиції")
       );
       setDiscountedProducts(discounted);
 
-      const bestSelling = items?.filter(
-        (item) => item.additionalCategory.includes("Хіти продаж")
+      const bestSelling = items?.filter((item) =>
+        item.additionalCategory.includes("Хіти продаж")
       );
       setBestSellingProducts(bestSelling);
 
-      const newProducts = items?.filter(
-        (item) => item.additionalCategory.includes("Новинки")
+      const newProducts = items?.filter((item) =>
+        item.additionalCategory.includes("Новинки")
       );
       setNewArrivals(newProducts);
     }
   }, [data]);
 
+  const paginate = (pageNumber, category) => {
+    setCurrentPage((prevCurrentPage) => ({
+      ...prevCurrentPage,
+      [category]: pageNumber,
+    }));
+  };
+
+  const getCurrentProducts = (products, category, pageNumber) => {
+    
+    if (!products) {
+      return [];
+    }
+    console.log("discountedProducts", products)
+    const indexOfLastProduct = pageNumber * productsPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+    return products.slice(indexOfFirstProduct, indexOfLastProduct);
+  };
+
   return (
     <>
       <Header>Кращі цінові пропозиції</Header>
+      <CustomPagination
+        productsPerPage={productsPerPage}
+        totalProducts={discountedProducts.length}
+        paginate={paginate}
+        category="discounted"
+      />
       <CardProduct>
-        {discountedProducts.length > 0 ? (
-          discountedProducts.map((item) => (
+        {discountedProducts && discountedProducts.length > 0 ? (
+          getCurrentProducts(discountedProducts, "discounted", currentPage.discounted).map((item) => (
             <ProductCard
               key={item._id}
               id={item._id}
@@ -69,9 +98,15 @@ export const CardsProducts = () => {
       </CardProduct>
 
       <Header>Хіти продаж</Header>
+      <CustomPagination
+        productsPerPage={productsPerPage}
+        totalProducts={bestSellingProducts.length}
+        paginate={paginate}
+        category="bestSelling"
+      />
       <CardProduct>
-        {bestSellingProducts.length > 0 ? (
-          bestSellingProducts.map((item) => (
+        {bestSellingProducts && bestSellingProducts.length > 0 ? (
+          getCurrentProducts(bestSellingProducts, "bestSelling", currentPage.bestSelling).map((item) => (
             <ProductCard
               key={item._id}
               name={item.name}
@@ -88,9 +123,15 @@ export const CardsProducts = () => {
       </CardProduct>
 
       <Header>Новинки</Header>
+      <CustomPagination
+        productsPerPage={productsPerPage}
+        totalProducts={newArrivals.length}
+        paginate={paginate}
+        category="newArrivals"
+      />
       <CardProduct>
-        {newArrivals.length > 0 ? (
-          newArrivals.map((item) => (
+        {newArrivals && newArrivals.length > 0 ? (
+          getCurrentProducts(newArrivals, "newArrivals", currentPage.newArrivals).map((item) => (
             <ProductCard
               key={item._id}
               name={item.name}
---
+++ src/components/CustomPagination/CustomPagination.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { Pagination } from '@mui/material';
+
+export const CustomPagination = ({ productsPerPage, totalProducts, paginate, category }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = Math.ceil(totalProducts / productsPerPage);
+
+  const handlePageChange = (event, page) => {
+    setCurrentPage(page);
+    paginate(page, category);
+  };
+
+  return (
+    <Pagination
+      count={totalPages}
+      page={currentPage}
+      onChange={handlePageChange}
+      variant="outlined"
+      shape="rounded"
+    />
+  );
+};
